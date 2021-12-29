57bc73abf5fedf4827e922b4bbed5537b457ca1d
Huy Nguyen <huynguyen260398@gmail.com>
2021-12-29 22:13:29 +0700

    Using the DbContext.

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,11 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Server.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options => 
+    options.UseSqlite(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -17,6 +24,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseStaticFiles();
 
 app.UseAuthorization();
 
