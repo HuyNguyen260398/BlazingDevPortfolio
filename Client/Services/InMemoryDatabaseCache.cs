9b48627d91f154ec47ac510c0fe0cd9e5c7f0b14
Huy Nguyen <huynguyen260398@gmail.com>
2021-12-30 21:51:17 +0700

    Consuming the API from client.

---
+++ Client/Services/InMemoryDatabaseCache.cs
@@ -0,0 +1,29 @@
+﻿using Shared.Models;
+
+namespace Client.Services;
+
+internal sealed class InMemoryDatabaseCache
+{
+    private readonly HttpClient _httpClient;
+
+    public InMemoryDatabaseCache(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    private List<Category> categories = null;
+
+    internal List<Category> Categories
+    {
+        get { return categories; }
+        set 
+        { 
+            categories = value;
+            NotifyCategoriesDataChanged();
+        }
+    }
+
+    internal event Action OnCategoriesDataChanged;
+
+    private void NotifyCategoriesDataChanged() => OnCategoriesDataChanged?.Invoke();
+}
