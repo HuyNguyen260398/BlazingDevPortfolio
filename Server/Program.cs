81b96428db3cf67d2ee9adc2b32285ba5fb98658
Huy Nguyen <huynguyen260398@gmail.com>
2022-01-01 17:37:36 +0700

    Prepare Swagger for deployment.

--- Server/Program.cs
+++ Server/Program.cs
@@ -28,7 +28,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Blazing Dev Portfolio API");
+        options.RoutePrefix = String.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
