93ada32286a4b5697da2a03e513dec70c7cbee0a
Huy Nguyen <huynguyen260398@gmail.com>
2022-01-05 07:33:10 +0700

    Updating the database (1/2)

--- Server/Data/AppDbContext.cs
+++ Server/Data/AppDbContext.cs
@@ -11,11 +11,14 @@ public class AppDbContext : DbContext
     }
 
     public DbSet<Category> Categories { get; set; }
+    public DbSet<Post> Posts { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         // Call the base version of this method as well, else we get an error later on
         base.OnModelCreating(modelBuilder);
+
+        #region Category seed
 
         Category[] categoriesToSeed = new Category[3];
 
@@ -31,5 +34,64 @@ public class AppDbContext : DbContext
         }
 
         modelBuilder.Entity<Category>().HasData(categoriesToSeed);
+
+        #endregion
+
+        modelBuilder.Entity<Post>(entity =>
+        {
+            entity.HasOne(p => p.Category)
+                  .WithMany(c => c.Posts)
+                  .HasForeignKey("CategoryId");
+        });
+
+        #region Post seed
+
+        Post[] postsToSeed = new Post[6];
+
+        for (int i = 1; i < 7; i++)
+        {
+            int categoryId = 0;
+
+            switch (i)
+            {
+                case 1:
+                    categoryId = 1;
+                    break;
+                case 2:
+                    categoryId = 1;
+                    break;
+                case 3:
+                    categoryId = 2;
+                    break;
+                case 4:
+                    categoryId = 2;
+                    break;
+                case 5:
+                    categoryId = 3;
+                    break;
+                case 6:
+                    categoryId = 3;
+                    break;
+                default:
+                    break;
+            }
+
+            postsToSeed[i - 1] = new Post
+            {
+                PostId = i,
+                ThumbnailImagePath = "/uploads/placeholder.jpg",
+                Title = $"Post {i}",
+                Excerpt = $"This is an excerpt for post {i}",
+                Content = String.Empty,
+                PublishDate = DateTime.UtcNow.ToString(),
+                IsPublished = true,
+                Author = "admin",
+                CategoryId = categoryId,
+            };
+        }
+
+        modelBuilder.Entity<Post>().HasData(postsToSeed);
+
+        #endregion
     }
 }
