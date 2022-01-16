b59e761cfba755264d3213723cf765a8bdeaa857
Huy Nguyen <huynguyen260398@gmail.com>
2022-01-16 11:05:58 +0700

    Adding image uploading (1/2)

---
+++ Server/Controllers/ImageUploadController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Models;
+
+namespace Server.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class ImageUploadController : ControllerBase
+{
+    private readonly IWebHostEnvironment _webHostEnvironment;
+
+    public ImageUploadController(IWebHostEnvironment webHostEnvironment)
+    {
+        _webHostEnvironment = webHostEnvironment;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody] UploadedImage uploadedImage)
+    {
+        try
+        {
+            if (ModelState.IsValid)
+                return BadRequest(ModelState);
+            
+            if (uploadedImage.OldImagePath != string.Empty)
+            {
+                if (uploadedImage.OldImagePath != "uploads/placeholder/jpg")
+                {
+                    string oldImageName = uploadedImage.OldImagePath.Split('/').Last();
+                    System.IO.File.Delete($"{_webHostEnvironment.ContentRootPath}\\wwwroot\\uploads\\{oldImageName}");
+                }
+            }
+
+            string guid = Guid.NewGuid().ToString();
+            string imageName = guid + uploadedImage.NewImageExtension;
+            string fullImageFileSystemPath = $"{_webHostEnvironment.ContentRootPath}\\wwwroot\\uploads\\{imageName}";
+
+            FileStream fileStream = System.IO.File.Create(fullImageFileSystemPath);
+            byte[] imageAsByte = Convert.FromBase64String(uploadedImage.NewImageBase64Content);
+            await fileStream.WriteAsync(imageAsByte, 0, imageAsByte.Length);
+            fileStream.Close();
+
+            string relativePathWithoutTrailingSlashes = $"uploads/{imageName}";
+            return Created("Create", relativePathWithoutTrailingSlashes);
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500, e.Message);
+        }
+    }
+}
---
+++ Shared/Models/UploadedImage.cs
@@ -0,0 +1,11 @@
+﻿namespace Shared.Models;
+
+public class UploadedImage
+{
+    public string NewImageExtension { get; set; }
+
+    // Base64 is basically a string that represent binary
+    public string NewImageBase64Content { get; set; }
+
+    public string OldImagePath { get; set; }
+}
